16dd67023f6b545ff27afc0671f595929cfdf259
simone-scotto <simone.scottotkd@gmail.com>
2025-08-05 17:29:50 +0200

    movie lists page, and single one

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,35 +1,51 @@
-import Jumbotron  from "../components/Jumbotron"
+import Jumbotron from "../components/Jumbotron"
 import { useState } from "react";
+import { useEffect } from "react"
+import { Link } from "react-router-dom";
 
+export default function MoviesPage() {
+    const moviesUrl = `http://localhost:3070/api/movies`
+    const [movies, setMovies] = useState([])
+    useEffect(() => {
+        fetch(moviesUrl)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setMovies(data)
 
-
-
-export default function MoviesPage () {
-    const MoviesUrl = `http://localhost:3070/movies/`
-    [movies, setMovies] = useState([])
-    fetch(MoviesUrl)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-        setMovies(data);
-      });
-    
+            })
+    }, [])
     return (
         <>
-        <section>
-            <h1>Movies Page</h1>
-            <Jumbotron title="Libreria Film" text="Qui trovi tutti i migliori film recentiu e non, per richieste di pellicole da caricare contattaci!" cta="Contattaci!"/>
-            <div className="container">
-                <div className="row row-cols-1 row-cols-md-3 g-3">
-                    <div className="col">
-                        <div className="card">
+            <section>
+                <h1>Movies Page</h1>
+                <Jumbotron title="Libreria Film" text="Qui trovi tutti i migliori film recentiu e non, per richieste di pellicole da caricare contattaci!" cta="Contattaci!" />
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-md-3 g-3">
+                        {movies.map((movie) => {
+                            return (
+                                <div className="col" key={movie.id}>
+                                    <div className="card h-100 w-75">
+                                        <Link to={`/movies/${movie.id}`}>
+                                            <img src={movie.image} alt="image" className="card-img-top" />
+                                        </Link>
 
-                        </div>
+                                        <div className="card-body">
+                                            <h5 className="card-title d-inline">{movie.title}</h5>
+                                            <span className="ps-3">({movie.release_year})</span>
+                                            <p className="card-text">Directed by: {movie.director}</p>
+                                        </div>
+
+
+                                    </div>
+                                </div>
+
+
+                            )
+                        })}
                     </div>
                 </div>
-            </div>
-        </section>
-        
+            </section>
         </>
     )
 }
--- src/pages/SingleMoviePage.jsx
+++ src/pages/SingleMoviePage.jsx
@@ -1,10 +1,45 @@
-import Jumbotron  from "../components/Jumbotron"
+import Jumbotron from "../components/Jumbotron"
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
 
 
-export default function SingleMoviePage () {
+
+export default function SingleMoviePage() {
+
+    const { id } = useParams()
+    console.log(id);
+    const [movie, setMovie] = useState({})
+    const singleMovieUrl = `http://localhost:3070/api/movies/${id}`
+    console.log(singleMovieUrl);
+
+    useEffect(() => {
+        fetch(singleMovieUrl)
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data);
+                setMovie(data)
+
+            })
+    }, [])
+
     return (
         <>
-        <h1>Single Movie Page</h1>
+            <h1>Single Movie Page</h1>
+            <section>
+                <div className="container d-flex justify-content-evenly">
+                    <div>
+                        <h1 className="text-center">{movie.title}</h1>
+                        <p className="text-center">{movie.release_year}</p>
+                        <p>Directed by: {movie.director}</p>
+                        <p>Genre: {movie.genre}</p>
+                        <p>Plot: {movie.abstract}</p>
+                        <p>{"media voto delle recensioni?"}</p>
+                    </div>
+
+                    <img className="h-100" src={movie.image} alt={movie.title} />
+
+                </div>
+            </section>
         </>
     )
 }
